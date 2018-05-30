0fc4b8a5575c034c6613ca36b43783f56333b0e4
Irene <iychiangsd@gmail.com>
2018-05-30 15:49:56 -0700

    included overflow in history box

--- src/js/TopRow/TopRowComponents/History.jsx
+++ src/js/TopRow/TopRowComponents/History.jsx
@@ -4,9 +4,11 @@ export default class History extends Component {
 
   render() {
     return (
-        <div className='history'>
+      <div className='history'>
+        <div>
           <span className='pastwords'>Past words: </span> {this.props.history.map(word => <li key={JSON.stringify(word)}>{word}</li>)}
         </div>
+      </div>
     );
   }
 }
