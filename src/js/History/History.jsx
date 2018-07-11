64c1fa3b4b79e8f11228c461e62fb32435cbff7f
Irene <iychiangsd@gmail.com>
2018-07-10 21:07:03 -0700

    added grid to past words

--- src/js/History/History.jsx
+++ src/js/History/History.jsx
@@ -5,8 +5,8 @@ export default class History extends Component {
   render() {
     return (
       <div className='history'>
-        <div>
-          <span className='pastwords'></span> {this.props.history.map(word => <li key={JSON.stringify(word)}>{word}</li>)}
+        <div className='pastwords'>
+          {this.props.history.map(word => <li key={JSON.stringify(word)}>{word}</li>)}
         </div>
       </div>
     );
